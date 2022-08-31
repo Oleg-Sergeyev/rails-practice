98a11ca8e4be21d80f44098e24afb3e76f8a9517
Oleg Sergeyev <sergeyev_ov@mail.ru>
2022-09-01 01:19:58 +0300

    Поправил имена, плюс рубокоп

--- sneakers/get_passports_status.rb
+++ sneakers/get_passports_status.rb
@@ -1,17 +1,20 @@
+# frozen_string_literal: true
+
 require 'sneakers'
 require 'json'
 require 'date'
 
-class Processor
+# class Passport
+class Passport
   include Sneakers::Worker
   from_queue :docs
 
   def work(msg)
     doc = JSON.parse(msg)
-    if doc["type"] == "passport"
-	File.open('passports.log', 'a') do |f|
-	  f.write("#{DateTime.now()}: #{doc["number"]} - #{doc["status"]}\n")
-	end
+    if doc['type'] == 'passport'
+      File.open('passports.log', 'a') do |f|
+        f.write("#{DateTime.now}: #{doc['number']} - #{doc['status']}\n")
+      end
     end
     ack!
   end
